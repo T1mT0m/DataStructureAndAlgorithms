caff825fe3fa351495e76c241e899b0b6265d378
T1mT0m <bijuwarrier1@gmail.com>
2023-03-13 22:46:57 +0530

    New methods added in LinkedList

--- DatastructuresnAlgorithms/src/com/github/bwar/dataalgos/linkedlist/LinkedList.java
+++ DatastructuresnAlgorithms/src/com/github/bwar/dataalgos/linkedlist/LinkedList.java
@@ -19,16 +19,7 @@ public class LinkedList {
 		
 		Node(int value){
 			this.value = value;
-		}
-		
-	}
-	
-	public void printlist() {
-		Node temp = head;
-		while (temp !=null) {
-			System.out.println(temp.value);
-			temp = temp.next;
-		}
+		}	
 	}
 	
 	public void getHead() {
@@ -41,6 +32,14 @@ public class LinkedList {
 	
 	public void getLength() {
 		System.out.println("Length value= "+length);
+	}
+	
+	public void printlist() {
+		Node temp = head;
+		while (temp !=null) {
+			System.out.println(temp.value);
+			temp = temp.next;
+		}
 	}
 	
 	public void append(int value) {
@@ -56,8 +55,8 @@ public class LinkedList {
 		
 	}
 	
-	
 	public Node removeLast() {
+	
 	 // when linkedList does not contain any node
 	 if (length == 0) return null;
 	 
@@ -77,11 +76,80 @@ public class LinkedList {
 		 head = null;
 		 tail = null;
 	 }
-	 
 	 return temp;
 	}
 	
-
+	
+	public void prepend(int value) {
+		
+		Node newNode = new Node(value);
+		if (length == 0) {
+			head = newNode;
+			tail = newNode;
+		}else {
+			newNode.next = head;
+			head = newNode;
+		}
+		
+		length++;
+		
+	}
+	
+	public Node removeFirst() {
+		
+		if (length == 0) return null;
+		Node temp = head;
+		head = head.next;
+		temp.next = null;
+		length--;
+		if (length == 0) {
+			tail = null;
+		}
+		return temp;	
+	}
+	
+	public Node get(int index) {
+		
+		if (index < 0 || index >= length) {
+			return null;
+		}
+		Node temp = head;
+		for(int i=0; i< index; i++) {
+			temp = temp.next;
+		}
+		return temp;
+	}
+	
+	
+	public boolean set(int index, int value) {
+		Node temp = get(index);
+		if (temp != null) {
+			temp.value = value;
+			return true;
+		}
+		return false;
+	}
+	
+	
+	public boolean insert(int index,int value) {
+		
+		if (index < 0 || index > length) return false;
+		if (index == 0) {
+			prepend(value);
+			return true;
+		}
+		if (index == length) {
+			append(value);
+			return true;
+		}
+		Node newNode = new Node(value);
+		Node temp = get(index-1);
+		newNode.next = temp.next;
+		temp.next = newNode;
+		length++;
+		return true;
+	}
+	
 
 
 }
--- DatastructuresnAlgorithms/src/com/github/bwar/dataalgos/linkedlist/Main.java
+++ DatastructuresnAlgorithms/src/com/github/bwar/dataalgos/linkedlist/Main.java
@@ -16,6 +16,24 @@ public class Main {
 		System.out.println(myLL.removeLast());
 		System.out.println(myLL.removeLast());
 		System.out.println(myLL.removeLast());
+		
+		myLL.append(1);
+		myLL.append(2);
+		myLL.append(3);
+		myLL.append(4);
+		myLL.prepend(0);
+		myLL.printlist();
+		myLL.getHead();
+		
+		myLL.removeFirst();
+		myLL.printlist();
+		//myLL.getHead();
+		
+		//System.out.println(myLL.get(3).value);
+		//myLL.getHead();
+		myLL.insert(2, 340);
+		myLL.printlist();
+		
 	}
 
 	
